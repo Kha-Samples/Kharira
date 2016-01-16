843208a98bcd7d3048172ed7cf8b8fb308b91802
Christian Reuter <christian@aoe-maniac.de>
2016-01-16 21:46:04 +0100

    First version with basic rendering.

--- Sources/KhajakTest.hx
+++ Sources/KhajakTest.hx
@@ -1,19 +1,42 @@
 package;
 
+import kha.Assets;
+import kha.Color;
 import kha.Framebuffer;
+import kha.math.FastVector3;
 import kha.Scheduler;
 import kha.System;
+import khajak.Mesh;
+import khajak.Renderer;
+import khajak.RenderObject;
 
 class KhajakTest {
+	var boxMesh: Mesh;
+	
 	public function new() {
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
+		
+		Renderer.init(new Renderer(Color.fromFloats(0.5, 0.5, 0.5)));
+		
+		Assets.loadEverything(loadFinished);
+	}
+	
+	function loadFinished() {
+		Renderer.the.light1.position = new FastVector3(1, 1, 1);
+		Renderer.the.light1.power = 100;
+		Renderer.the.light1.color = Color.White;
+		
+		boxMesh = new Mesh(Assets.blobs.cube_obj.toString());
+		var box = new RenderObject(boxMesh, Color.Black, Assets.images.cube);
+		Renderer.the.objects.push(box);
 	}
 
 	function update(): Void {
 		
 	}
 
-	function render(framebuffer: Framebuffer): Void {		
+	function render(framebuffer: Framebuffer): Void {
+		Renderer.the.render(framebuffer);
 	}
 }
--- khafile.js
+++ khafile.js
@@ -2,4 +2,5 @@ var project = new Project('KhajakTest');
 project.addLibrary('Khajak');
 project.addAssets('Assets/**');
 project.addSources('Sources');
+project.addShaders('Libraries/Khajak/Sources/khajak/Shaders/**');
 return project;
