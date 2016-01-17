30042dce46d8f5332bb068a8e2f9d0208fcebd3b
Robert Konrad <robert@ktx-software.com>
2016-01-17 02:58:55 +0100

    Added haxebullet submodule and proto index.html file (copy that and ammo.js to build/html5).

--- khafile.js
+++ khafile.js
@@ -1,5 +1,6 @@
 var project = new Project('KhajakTest');
 project.addLibrary('Khajak');
+project.addLibrary('haxebullet');
 project.addAssets('Assets/**');
 project.addSources('Sources');
 project.addShaders('Libraries/Khajak/Sources/khajak/Shaders/**');
