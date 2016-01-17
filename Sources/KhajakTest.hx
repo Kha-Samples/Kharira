b1baa9ae2874d0e66aca98d87534c77f0a7d965d
Christian Reuter <christian@aoe-maniac.de>
2016-01-17 23:52:03 +0100

    Nicer particles.

--- Sources/KhajakTest.hx
+++ Sources/KhajakTest.hx
@@ -20,8 +20,6 @@ class KhajakTest {
 	var billboardMesh: Mesh;
 	var box: RenderObject;
 	var initialized: Bool;
-	var emitter: Emitter;
-	var emitter2: Emitter;
 	
 	public function new() {
 		System.notifyOnRender(render);
@@ -37,18 +35,17 @@ class KhajakTest {
 		Renderer.the.light1.power = 100;
 		Renderer.the.light1.color = Color.White;
 		
-		boxMesh = new Mesh(Assets.blobs.cube_obj.toString());
+		boxMesh = Mesh.FromModel(Assets.blobs.cube_obj.toString());
 		box = new RenderObject(boxMesh, Color.Black, Assets.images.cube);
 		Renderer.the.objects.push(box);
 		
-		billboardMesh = new Mesh(Assets.blobs.billboard_obj.toString());
-		emitter = new Emitter(new FastVector3(-1, 0, 2), new FastVector3(1, 0, 0), 45, false, new Vector2(2, 3), new Vector2(0.5, 1), new FastVector2(0.1, 0.1), new FastVector2(0.2, 0.2), Assets.images.cube, 0.3, 10, billboardMesh);
-		emitter.start(10);
+		var emitter = new Emitter(new FastVector3(0, -1, 2), 0.1, true, new FastVector3(0, 1, 0), 0.125 * Math.PI, false, new Vector2(1, 1.5), new Vector2(1, 1), new Vector2(1, 1), new FastVector2(0.15, 0.15), new FastVector2(0.25, 0.25), Assets.images.smoke, 0.005, 0.005, 500);
+		emitter.start(Math.POSITIVE_INFINITY);
 		Renderer.the.particleEmitters.push(emitter);
 		
-		emitter2 = new Emitter(new FastVector3(1, 0, -2), new FastVector3(-1, 0, 0), 45, false, new Vector2(2, 3), new Vector2(0.5, 1), new FastVector2(0.1, 0.1), new FastVector2(0.2, 0.2), Assets.images.cube, 0.3, 10, billboardMesh);
-		emitter2.start(10);
-		Renderer.the.particleEmitters.push(emitter2);
+		emitter = new Emitter(new FastVector3(1, 0, -2), 0, false, new FastVector3(-1, 0, 0), 0, true, new Vector2(1, 1), new Vector2(0.5, 1), new Vector2(0.5, 1), new FastVector2(0.1, 0.1), new FastVector2(0.2, 0.2), Assets.images.cube, 0.3, 0.5, 10);
+		emitter.start(10);
+		Renderer.the.particleEmitters.push(emitter);
 		
 		lastTime = Scheduler.time();
 		initialized = true;
@@ -60,8 +57,9 @@ class KhajakTest {
 		var deltaTime = Scheduler.time() - lastTime;
 		lastTime = Scheduler.time();
 		
-		emitter.update(deltaTime);
-		emitter2.update(deltaTime);
+		for (emitter in Renderer.the.particleEmitters) {
+			emitter.update(deltaTime);
+		}
 		Renderer.the.updateCamera(new FastVector3(0, 0, 10), new FastVector3(0, 0, 0));
 	}
 
