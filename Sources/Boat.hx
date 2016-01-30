763682858ca019d3c5a090f391f0942b6ad7cd93
Christian Reuter <gamedev@aoe-maniac.de>
2016-01-30 13:46:54 +0100

    Added rotation to boat physics.

--- Sources/Boat.hx
+++ Sources/Boat.hx
@@ -1,36 +1,51 @@
 package;
 
+import kha.math.Matrix4;
 import kha.math.FastMatrix4;
 import kha.math.Vector3;
+import kha.math.Vector4;
 import khajak.RenderObject;
 
 class Boat extends RenderObject {
 	
 	public static var IMPULSE_DAMPING = 0.5;
+	public static var IMPULSE_SPEED = 10.0;
 	public static var ROTATING_DAMPING = 0.5;
+	public static var ROTATING_SPEED = 1.0 * Math.PI;
 	
-	public var position(default, null): Vector3;
-	private var impulse: Vector3;
-	private var rotation: Vector3;
+	public var position(default, null): Vector4;
+	public var direction(default, null): Vector4;
 	
-	public function new(position: Vector3) {
+	private var angle: Float;
+	private var impulse: Vector4;
+	private var rotationDir: Float;
+	private var rotationStrength: Float;
+	
+	public function new(position: Vector4) {
 		super(Meshes.Boat, kha.Color.Black, kha.Assets.images.cube);
 		
 		this.position = position;
-		impulse = new Vector3();
-		rotation = new Vector3();
+		angle = 0;
+		direction = new Vector4(0, 0, 1);
+		impulse = new Vector4();
+		rotationDir = 0;
+		rotationStrength = 0;
 	}
 	
 	public function addImpulse(strenght: Float, left: Bool) {
-		impulse = impulse.add(new Vector3(0, 0, strenght));
+		rotationStrength = strenght;
+		rotationDir = (left ? -1 : 1) * rotationStrength;
+		direction = Matrix4.rotationY(angle).multvec(new Vector4(0, 0, 1));
+		impulse = impulse.add(direction.mult(strenght));
 	}
 	
 	public function update(deltaTime: Float) {
 		impulse.length = Math.max(impulse.length * (1 - IMPULSE_DAMPING * deltaTime), 0);
-		rotation.length = Math.max(rotation.length * (1 - ROTATING_DAMPING * deltaTime), 0);
+		rotationStrength =  Math.max(rotationStrength * (1 - ROTATING_DAMPING * deltaTime), 0);
 		
-		position = position.add(impulse);
+		angle += rotationDir * rotationStrength * ROTATING_SPEED * deltaTime;
+		position = position.add(impulse.mult(IMPULSE_SPEED * deltaTime));
 		
-		model = FastMatrix4.translation(position.x, position.y, position.z);
+		model = FastMatrix4.translation(position.x, position.y, position.z).multmat(FastMatrix4.rotationY(angle));
 	}
 }
