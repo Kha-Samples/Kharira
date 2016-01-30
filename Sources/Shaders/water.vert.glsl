7bd973900d6094ed20588d9c4745cac0a8b566da
Robert Konrad <robert@ktx-software.com>
2016-01-30 13:05:45 +0100

    Changed water vertex shader so it makes more sense.

--- Sources/Shaders/water.vert.glsl
+++ Sources/Shaders/water.vert.glsl
@@ -4,6 +4,7 @@ uniform sampler2D tex;
 
 attribute vec2 pos;
 
-void main() {  
-	gl_Position = vec4(texture2D(tex, pos).xy, 0.0, 1.0);
+void main() {
+	vec4 coord = texture2D(tex, pos);
+	gl_Position = vec4(pos.x, coord.r, pos.y, 1.0);
 }
